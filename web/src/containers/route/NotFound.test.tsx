1cbb91c432e2ea822902350657ed90475eb8a495
Django Cass <django@dcas.dev>
2022-03-27 14:22:52 +1100

    Removed broken tests

--- web/src/containers/route/NotFound.test.tsx
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-import React from "react";
-import NotFound from "./NotFound";
-
-it("renders correctly", () => {
-	const tree = renderer.create(<NotFound/>).toJSON();
-	expect(tree).toMatchSnapshot();
-});
